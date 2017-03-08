46a0ad501518d852619e2263cea876ffc54e7e4f
psprogis <pavel.sprogis@gmail.com>
2017-03-08 09:41:00 +0200

    Add custom Angular Material theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MdIconRegistry } from '@angular/material';
 
 @Component({
-  selector: 'ngs-root',
+  selector: 'ngs-app',
+  styleUrls: [ './app.component.scss' ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngs works!';
+
+  constructor(
+      private domSanitizer: DomSanitizer,
+      private iconRegistry: MdIconRegistry) {
+    this.registerIcons(new Map<string, string>([
+      [ 'logo', 'assets/ngshop-logo.svg' ]
+    ]));
+  }
+
+  private registerIcons(icons: Map<string, string>) {
+    icons.forEach((url, id) => {
+      const safeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+      this.iconRegistry.addSvgIconInNamespace('ngs', id, safeUrl);
+    });
+  }
 }
