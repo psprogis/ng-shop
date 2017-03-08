8008d92b89cb92187e4d0047b29f451a070300b5
psprogis <pavel.sprogis@gmail.com>
2017-03-08 22:38:16 +0200

    Make categories tabs interactive

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,8 +8,21 @@ import { Product, ProductService } from '../shared/services';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+    private readonly categories = [
+    'all',
+    'featured',
+    'latest',
+    'sport'
+    ];
+
   products: Observable<Product[]>;
   constructor(private productService: ProductService) {
     this.products = this.productService.getAll();
+  }
+
+  onTabChange(tabIndex: number) {
+    const category = this.categories[tabIndex];
+    this.products = this.productService.getCategory(category);
   }
 }
