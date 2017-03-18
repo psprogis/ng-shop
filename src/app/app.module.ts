f173f902235d1f35210c95ef20bd2f0ab2121afd
psprogis <pavel.sprogis@gmail.com>
2017-03-18 11:16:13 +0200

    Add product page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,18 @@ import { MaterialModule } from '@angular/material';
 
 import { ProductService } from './shared/services';
 import { HomeComponent, ProductTileComponent } from './home';
-import { ProductComponent } from './product/product.component';
 
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routing';
+import { ProductComponent, ProductDetailsComponent } from './product';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProductTileComponent,
-    ProductComponent
+    ProductComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
