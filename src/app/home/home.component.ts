b9ccac965d76296119198fdf9c1d7d5fca92c88f
psprogis <pavel.sprogis@gmail.com>
2017-03-18 13:52:09 +0200

    Making category tabs routable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,28 +1,49 @@
-import { Component } from '@angular/core';
+import { AfterViewInit,
+  ViewChild,
+  OnDestroy,
+  Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MdTabGroup } from '@angular/material';
 import { Observable } from 'rxjs/Observable';
 import { Product, ProductService } from '../shared/services';
+
 
 @Component({
   selector: 'ngs-home',
   styleUrls: [ './home.component.scss' ],
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+
+    @ViewChild(MdTabGroup) mdTabGroup: MdTabGroup;
 
     private readonly categories = [
-    'all',
-    'featured',
-    'latest',
-    'sport'
+      'all',
+      'featured',
+      'latest',
+      'sport'
     ];
 
   products: Observable<Product[]>;
-  constructor(private productService: ProductService) {
-    this.products = this.productService.getAll();
+  constructor(private productService: ProductService,
+      private route: ActivatedRoute,
+      private router: Router) {
+    this.products = this.route.params
+    // Parameters list below uses the ES6 feature called destructuring.
+    .switchMap(({category}) => {
+      return category === 'all' ?
+        this.productService.getAll() :
+        this.productService.getCategory(category);
+    });
   }
 
   onTabChange(tabIndex: number) {
     const category = this.categories[tabIndex];
-    this.products = this.productService.getCategory(category);
+    this.router.navigate([category], { relativeTo: this.route.parent });
+  }
+
+  ngAfterViewInit() {
+    const category = this.route.snapshot.params['category'];
+    // this.mdTabGroup.selectedIndex = this.categories.indexOf(category);
   }
 }
